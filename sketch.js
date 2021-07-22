444864a8ce19a3a4d806301de61ec167441738d9
himagupta07 <87651640+himagupta07@users.noreply.github.com>
2021-07-22 22:29:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var sea;
+var ship;
+var ship_moving;
+var seaImage;
+
+function preload(){
+ship_moving= loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
+seaImage= loadImage("sea.png");
+}
+
+function setup(){
+  createCanvas(800,600);
+  ship= createSprite(400,250,10, 10);
+  ship.addAnimation("moving", ship_moving);
+  ship.scale= 0.5;
+  sea= createSprite(200,180,400,20);
+  sea.addImage("sea", seaImage);
+  
+  createEdgeSprites();
+}
+
+function draw() {
+background("grey");
+
+sea.velocityX= -5;
+if(sea.x < 0){
+  sea.x= sea.width/2;
+}
+
+console.log(ship.x);
+
+drawSprites();
+}
